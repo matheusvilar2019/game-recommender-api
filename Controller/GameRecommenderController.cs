84b3b8738ea16e97d826bba1cf13ac9fc0ac34ee
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2025-07-12 15:06:02 -0300

    basic API consumer

---
+++ Controller/GameRecommenderController.cs
@@ -0,0 +1,44 @@
+﻿using GameRecommenderAPI.Dtos;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace GameRecommenderAPI.Controller
+{
+    [ApiController]
+    public class GameRecommenderController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GameRecommenderController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("v1/recommender")]
+        public async Task<IActionResult> Recommender([FromBody] ParametersDto parameters)
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync("https://www.freetogame.com/api/games?platform=pc");
+
+                if (!response.IsSuccessStatusCode)
+                    return StatusCode((int)response.StatusCode, "Erro ao buscar dados da FreeToGame");
+
+                var json = await response.Content.ReadAsStringAsync();
+                var games = JsonSerializer.Deserialize<List<GameDto>>(json);
+
+                return Ok(games);
+            } 
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+            
+        }
+    }
+}
---
+++ Dtos/ParametersDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameRecommenderAPI.Dtos
+{
+    public class ParametersDto
+    {
+        public string Category { get; set; }
+        public string Platform { get; set; }
+        public int RamMemory { get; set; }
+    }
+}
